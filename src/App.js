5765de634b027e4919b80fce48986b3d46931909
Elias <eeliasmaia@gmail.com>
2019-06-13 12:24:07 -0300

    Create the app Header, add styles to the header and to the whole app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Rocketseat</h1>
-    </div>
-  );
-}
+import Header from './components/Header';
+
+import './styles.css';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
