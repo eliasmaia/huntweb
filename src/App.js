e41be7c5bb0fbcf83d2f526ebf2db69234f4cdcb
Elias <eeliasmaia@gmail.com>
2019-06-13 13:15:05 -0300

    Start to get responses from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './styles.css';
 
 import Header from './components/Header';
-
-import './styles.css';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
